7fbf45dfb132e1e73f51b13d0c663d6de85bf652
tttosnown <162364812+tttosnown@users.noreply.github.com>
2026-09-11 14:03:23 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='checky-v2-docx-20260911';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./checky-data.json']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
